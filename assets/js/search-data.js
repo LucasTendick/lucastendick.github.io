884646f5b47eca8393a45c3b2d25b0eca3981da7
LucasTendick <52408382+LucasTendick@users.noreply.github.com>
2026-04-29 11:57:22 +0000

    Deploying to gh-pages from @ LucasTendick/lucastendick.github.io@1e06c68cf1681e33867f90275240999cc3acf0f6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -499,18 +499,18 @@ ninja.data = [{
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%6C%75%63%61%73-%61%6D%61%64%65%75%73.%74%65%6E%64%69%63%6B@%69%6E%72%69%61.%66%72", "_blank");
+        },
+      },{
         id: 'social-cv',
         title: 'CV',
         section: 'Socials',
         handler: () => {
           window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
