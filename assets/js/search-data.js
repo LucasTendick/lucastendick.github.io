d2c2c114df9ff90807d2220649b116074da5f1ff
LucasTendick <52408382+LucasTendick@users.noreply.github.com>
2026-05-15 13:58:05 +0000

    Deploying to gh-pages from @ LucasTendick/lucastendick.github.io@e55b35b2179c415ab995c608bcf339aeed8a694f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a summary of my CV. You can find my complete CV [here](/assets/pdf/LucasTendick_CV.pdf).",
+          description: "This is a summary of my CV. You can find my complete CV [here](/assets/pdf/PhDThesis.pdf).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
