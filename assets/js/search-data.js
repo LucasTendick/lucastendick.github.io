26ad2f58a11682ea927f39a00d43de4b7175c9e5
LucasTendick <52408382+LucasTendick@users.noreply.github.com>
2026-06-12 09:51:56 +0000

    Deploying to gh-pages from @ LucasTendick/lucastendick.github.io@8e836c6ad055026ea2bb1d9aa111774212353b41 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -110,6 +110,17 @@ ninja.data = [{
             window.location.href = "/blog/2024/post-citation/";
           
         },
+      },{id: "post-a-post-with-pseudo-code",
+        
+          title: "a post with pseudo code",
+        
+        description: "this is what included pseudo code could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2024/pseudocode/";
+          
+        },
       },{id: "post-a-post-with-giscus-comments",
         
           title: "a post with giscus comments",
